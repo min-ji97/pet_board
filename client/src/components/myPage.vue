294ae28b9f3d8136606e504ca1bea37ae08ca23a
jominji <whalswl576@gmail.com>
2022-03-18 23:35:50 +0900

    (client): myPage 생성

---
+++ client/src/components/myPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        
+        <div class="preview-container">
+            <Preview/> 
+        </div>
+
+        
+          
+    </div>
+</template>
+
+<script>
+import Preview from '@/components/Preview'
+export default {
+    name: 'App',
+    components: {
+       Preview
+    },
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        window.scrollTo(0,0);
+        //새로고침
+        window.onload = ()=>{
+            setTimeout(()=>{
+                scrollTo(0,0);
+            },100);
+        }
+    },
+    methods: {
+        
+
+    } 
+    
+}
+
+</script>
+<style scoped>
+   
+
+
+
+
+</style>
